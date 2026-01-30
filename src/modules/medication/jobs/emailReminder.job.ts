81571efa0afe3be3039e5ee6f6580e0a0911f391
Atul Kaple <kapleatul@gmail.com>
2026-01-31 02:19:50 +0530

    xyz

--- src/modules/medication/jobs/emailReminder.job.ts
+++ src/modules/medication/jobs/emailReminder.job.ts
@@ -2,16 +2,7 @@ import { getCognitoUserEmail } from "../../../aws/cognitoUser.service";
 import MedicationDose from "../models/MedicationDose";
 import { sendDoseReminder } from "../services/notification.service";
 
-let isJobRunning = false;
-
 export async function runEmailReminderJob() {
-  // 2. If running, skip this cycle
-  if (isJobRunning) {
-    console.log("⚠️ Email job already running. Skipping this cycle.");
-    return;
-  }
-
-  isJobRunning = true; // Lock
   const now = new Date();
 
   console.log("📧 Email Reminder Job START", now.toISOString());
@@ -76,6 +67,6 @@ export async function runEmailReminderJob() {
 
     console.log("🔐 emailSentAt saved");
   }
-isJobRunning = false;
+
   console.log("📧 Email Reminder Job END");
 }
